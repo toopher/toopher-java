e7c235f2ae0c5671dbd9579cbd31fb052302561a
Thomas Darr <me@trdarr.com>
2014-03-24 18:14:02 -0500

    Mavenize tests

--- src/test/java/com/toopher/TestToopherAPI.java
+++ src/test/java/com/toopher/TestToopherAPI.java
@@ -1,38 +1,48 @@
-package com.toopher;
+package com.toopher.test;
 
-import org.junit.Test;
+import com.toopher.*;
+import org.junit.*;
+import java.net.*;
 
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-import java.net.URL;
-
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class TestToopherAPI {
-    private boolean isValidURL(String url) {
-        try {
-            new URL(url).toURI();
-        } catch (MalformedURLException e) {
-            return false;
-        } catch (URISyntaxException e) {
-            return false;
-        }
+    @Test
+    public void testCreatePairing() throws InterruptedException, RequestError {
+        HttpClientMock httpClient = new HttpClientMock(200,
+                "{'id':'1','enabled':true,'user':{'id':'1','name':'some user'}}".replace("'", "\""));
 
-        return true;
+        ToopherAPI toopherApi = new ToopherAPI("key", "secret",
+                createURI("https://api.toopher.test/v1"), httpClient);
+        PairingStatus pairing = toopherApi.pair("awkward turtle", "some user");
+
+        assertEquals(httpClient.getLastCalledMethod(), "POST");
+        assertEquals(httpClient.getLastCalledData("pairing_phrase"), "awkward turtle");
+    }
+
+    private URI createURI(String url) {
+        try {
+            return new URL(url).toURI();
+        } catch (MalformedURLException e) {
+            return null;
+        } catch (URISyntaxException e) {
+            return null;
+        }
+    }
+
+    private boolean isValidURL(String url) {
+        return createURI(url) != null;
     }
 
     @Test
     public void testBaseURL() {
         assertNotNull("Base URL is null.", ToopherAPI.getBaseURL());
         assertTrue("Base URL is not valid.",
-                isValidURL(ToopherAPI.getBaseURL()));
+                   isValidURL(ToopherAPI.getBaseURL()));
     }
 
     @Test
     public void testVersion() {
         assertNotNull("Version is not null.", ToopherAPI.VERSION);
-        assertTrue("True is not true.", true);
     }
-
 }

6d4fcb3b375f1969b8941a591f452ace11063df9
Grace Yim <grace@toopher.com>
2015-02-06 14:47:41 -0600

    Add javadocs and clean up UserTerminal

--- src/main/java/com/toopher/UserTerminal.java
+++ src/main/java/com/toopher/UserTerminal.java
@@ -8,10 +8,29 @@ import org.json.JSONObject;
  *
  */
 public class UserTerminal extends ApiResponseObject {
+    /**
+     * The ToopherAPI object associated with this user terminal
+     */
     public ToopherAPI api;
+
+    /**
+     * The unique id for the user terminal
+     */
     public String id;
+
+    /**
+     * The name of the user terminal
+     */
     public String name;
+
+    /**
+     * The unique id specified by the requester
+     */
     public String requesterSpecifiedId;
+
+    /**
+     * The User object associated with the user terminal
+     */
     public User user;
 
     public UserTerminal (JSONObject json, ToopherAPI toopherAPI) throws JSONException {
@@ -20,29 +39,37 @@ public class UserTerminal extends ApiResponseObject {
         this.api = toopherAPI;
         this.id = json.getString("id");
         this.name = json.getString("name");
-        if (json.has("name_extra")) {
-            this.requesterSpecifiedId = json.getString("name_extra");
-        }
+        this.requesterSpecifiedId = json.getString("name_extra");
         this.user = new User(json.getJSONObject("user"), toopherAPI);
     }
 
     @Override
     public String toString() {
-        return String.format("[UserTerminal: id=%s; name=%s; requesterSpecifiedId=%s; userName=%s; userId=%s]",
-                id, name, requesterSpecifiedId, user.name, user.id);
+        return String.format("[UserTerminal: id=%s; name=%s; requesterSpecifiedId=%s; userName=%s; userId=%s; userToopherAuthenticationEnabled=%b]",
+                id, name, requesterSpecifiedId, user.name, user.id, user.toopherAuthenticationEnabled);
     }
 
+    /**
+     * Update the UserTerminal object with JSON response from the server
+     *
+     * @throws RequestError
+     *          Thrown when an exceptional condition is encountered
+     */
     public void refreshFromServer() throws RequestError {
         String endpoint = String.format("user_terminals/%s", id);
         JSONObject result = api.advanced.raw.get(endpoint);
         update(result);
     }
 
+    /**
+     * Update the UserTerminal object with JSON response
+     *
+     * @param jsonResponse
+     *          The JSON response received from the server
+     */
     public void update(JSONObject jsonResponse) {
         this.name = jsonResponse.getString("name");
-        if (jsonResponse.has("name_extra")) {
-            this.requesterSpecifiedId = jsonResponse.getString("name_extra");
-        }
+        this.requesterSpecifiedId = jsonResponse.getString("name_extra");
         this.user.update(jsonResponse.getJSONObject("user"));
     }
 }

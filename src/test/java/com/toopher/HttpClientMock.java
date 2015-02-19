6934eedb80630aa19a99f06a7e6057218b7cc0bf
Grace Yim <grace@toopher.com>
2015-02-19 16:04:11 -0600

    Remove expectedUriResponses since Users.getByName only needs one response

--- src/test/java/com/toopher/HttpClientMock.java
+++ src/test/java/com/toopher/HttpClientMock.java
@@ -33,21 +33,11 @@ public class HttpClientMock extends DefaultHttpClient {
     private HttpUriRequest lastRequest;
     private int expectedResponseStatus;
     private String expectedResponseBody;
-    private Map<URI, ResponseMock> expectedUriResponses;
     private URI lastURI;
 
     public HttpClientMock(int responseStatus, String responseBody) throws InterruptedException {
         this.expectedResponseStatus = responseStatus;
         this.expectedResponseBody = responseBody;
-        done = new Semaphore(1);
-        done.acquire();
-    }
-
-    public HttpClientMock(Map<URI, ResponseMock> responses) throws InterruptedException {
-        expectedUriResponses = new HashMap<URI, ResponseMock>();
-        for (URI url : responses.keySet()) {
-            expectedUriResponses.put(url, responses.get(url));
-        }
         done = new Semaphore(1);
         done.acquire();
     }
@@ -77,16 +67,10 @@ public class HttpClientMock extends DefaultHttpClient {
     }
 
     public String getExpectedResponse() {
-        if (expectedUriResponses != null) {
-            return expectedUriResponses.get(lastURI).getResponseBody();
-        }
         return expectedResponseBody;
     }
 
     public int getExpectedResponseStatus() {
-        if (expectedUriResponses != null) {
-            return expectedUriResponses.get(lastURI).getStatusCode();
-        }
         return expectedResponseStatus;
     }
 
@@ -106,10 +90,6 @@ public class HttpClientMock extends DefaultHttpClient {
             lastParams = req.getParams();
         }
         BasicHttpEntity entity = new BasicHttpEntity();
-        if (req == lastRequest && expectedUriResponses != null) {
-            expectedResponseBody = expectedUriResponses.get(req.getURI()).getResponseBody();
-            expectedResponseStatus = expectedUriResponses.get(req.getURI()).getStatusCode();
-        }
         try {
             entity.setContent(new ByteArrayInputStream(expectedResponseBody.getBytes("UTF-8")));
         } catch (UnsupportedEncodingException e) {
